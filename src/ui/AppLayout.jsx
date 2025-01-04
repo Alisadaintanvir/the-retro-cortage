5e9a95b481bc73d2c186c110ce7e4f2fc5520ba8
Alisadaintanvir <alisadaintanvir@gmail.com>
2025-01-05 02:37:49 +0600

    add  new cabin form and validations

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,17 +9,28 @@ const StyledAppLayout = styled.div`
   grid-template-rows: auto 1fr;
   height: 100vh;
 `;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
 function AppLayout() {
   const Main = styled.main`
     background-color: var(--color-gray-50);
     padding: 4rem 4.8rem 6.4rem;
+    overflow: scroll;
   `;
   return (
     <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
